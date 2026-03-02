1857e3b7b0c8f0791f7b7a8dd262cc40383e3c5c
rnktlhr <rinakitluhur10@gmail.com>
2026-03-02 10:23:41 +0700

    menambahkan det mapping dengan path /api/user/{id} pada file UserController.java

--- src/main/java/com/deploy/praktikum1/controller/UserController.java
+++ src/main/java/com/deploy/praktikum1/controller/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -41,6 +38,19 @@ public class UserController {
     )
     public ResponseEntity<Map<String, Object>> getAllUser() {
         List<UserDto> result = userService.getAllUser();
+
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "status", "success",
+                "data", result
+        ));
+    }
+
+    @GetMapping(
+            path = "/api/users/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> getUserById(@PathVariable("id") String id) {
+        UserDto result = userService.getUserById(id);
 
         return ResponseEntity.status(HttpStatus.OK).body(Map.of(
                 "status", "success",
