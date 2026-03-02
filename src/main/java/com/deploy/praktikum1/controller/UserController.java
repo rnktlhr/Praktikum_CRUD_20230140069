9183cb3e452adb8cd64c684baba15a29d052a7f5
rnktlhr <rinakitluhur10@gmail.com>
2026-03-02 10:22:41 +0700

    menambahkan det mapping pada file UserController.java

--- src/main/java/com/deploy/praktikum1/controller/UserController.java
+++ src/main/java/com/deploy/praktikum1/controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,6 +30,19 @@ public class UserController {
         UserDto result = userService.AddUser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "status", "success",
+                "data", result
+        ));
+    }
+
+    @GetMapping(
+            path = "/api/users",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, Object>> getAllUser() {
+        List<UserDto> result = userService.getAllUser();
+
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
                 "status", "success",
                 "data", result
         ));
